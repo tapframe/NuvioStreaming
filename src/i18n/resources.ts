9b2b6191215af4dc327586cc37870bfac36b21be
tapframe <nayifveliya99@gmail.com>
2026-01-07 12:37:22 +0530

    added italian language to UI.

--- src/i18n/resources.ts
+++ src/i18n/resources.ts
@@ -3,6 +3,7 @@ import pt from './locales/pt.json';
 import ar from './locales/ar.json';
 import es from './locales/es.json';
 import fr from './locales/fr.json';
+import it from './locales/it.json';
 
 export const resources = {
     en: { translation: en },
@@ -10,4 +11,5 @@ export const resources = {
     ar: { translation: ar },
     es: { translation: es },
     fr: { translation: fr },
+    it: { translation: it },
 };
